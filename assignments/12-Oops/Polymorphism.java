ba0c66c544a82fd666d2aa97ba8bd72f9bf7d9a3
ravigorla2004-sketch <ravigorla2004@gmail.com>
2026-08-21 16:02:05 +0530

    Add method overloading to Calculator class
    Implement method overloading in Calculator class for different types and numbers of parameters.

---
+++ assignments/12-Oops/Polymorphism.java
@@ -0,0 +1,29 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("Sum of two numbers: "
+                + calculator.add(10, 20));
+
+        System.out.println("Sum of three numbers: "
+                + calculator.add(10, 20, 30));
+
+        System.out.println("Sum of decimal numbers: "
+                + calculator.add(10.5, 20.5));
+    }
+}
